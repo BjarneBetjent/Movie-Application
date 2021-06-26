683ab4534a1c43aee6abf1760d6af8884999a2ed
Olav <osmevoll@gmail.com>
2021-06-26 21:22:09 +0200

    change top padding

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 import App from './App';
 
-//axios.defaults.baseURL = "http://localhost:5001" 
+axios.defaults.baseURL = "http://localhost:5001" 
 /* const port = process.env.PORT || 5001;
 axios.defaults.baseURL = `https://movie-buffies.herokuapp.com:${port}`; */
 
